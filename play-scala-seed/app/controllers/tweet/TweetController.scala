23ddefbe341126af8988455ce4cd9b129287e3f8
kim dongjin <1222kdj@gmail.com>
2021-05-11 00:03:18 +0900

    commit

--- play-scala-seed/app/controllers/tweet/TweetController.scala
+++ play-scala-seed/app/controllers/tweet/TweetController.scala
@@ -9,14 +9,21 @@ import models.Tweet
 
 @Singleton
 class TweetController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+  // DBのMockとして利用したいので、先ほどlistに作成したインスタンスをフィールドとして定義し直す
+  val tweets: Seq[Tweet] = (1L to 10L).map(i => Tweet(Some(i), s"test tweet${i.toString}"))
 
   def list() = Action { implicit request: Request[AnyContent] =>
-    // 1から10までのTweetクラスのインタンスを作成しています。
-    // 1 to 10だとIntになってしまうので1L to 10LでLongにしています。
-    val tweets: Seq[Tweet] = (1L to 10L).map(i => Tweet(Some(i), s"test tweet${i.toString}"))
-
     // viewの引数としてtweetsを渡します。
     Ok(views.html.tweet.list(tweets))
+  }
+
+  def show(id: Long) = Action { implicit request: Request[AnyContent] =>
+    // idが存在して、値が一致する場合にfindが成立
+    tweets.find(_.id.exists(_ == id)) match {
+      case Some(tweet) => Ok(views.html.tweet.show(tweet))
+      // status codeを404にしつつページを返しています。
+      case None        => NotFound(views.html.error.page404())
+    }
   }
 
 }
